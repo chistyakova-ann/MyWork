135acaf25704e1fcc3ae211f73f77ba62309794c
chistyakova-ann <chistyakova_69@mail.ru>
2018-01-19 22:51:06 +0300

    .

--- include/lexer.h
+++ include/lexer.h
@@ -6,30 +6,8 @@
 #include "../include/location.h"
 #include <string>"
 
-enum lexem_code : unsigned short {
-    None,       Unknown,     sp,         
-    bp,         r0,          r1,         
-    r2,         r3,          r4,         
-    r5,         r6,          r7,         
-    r8,         r9,          r10,        
-    r11,        r12,         r13,        
-    r14,        r15,         r16,        
-    r17,        r18,         r19,        
-    r20,        r21,         r22,        
-    r23,        r24,         r25,        
-    r26,        r27,         r28,        
-    r29,        r30,         r31,        
-    f0,         f1,          f2,         
-    f3,         f4,          f5,         
-    f6,         f7,          f8,         
-    f9,         f10,         f11,        
-    f12,        f13,         f14,        
-    f15,        f16,         f17,        
-    f18,        f19,         f20,        
-    f21,        f22,         f23,        
-    f24,        f25,         f26,        
-    f27,        f28,         f29,        
-    f30,        f31,         Equal,      
+enum lexemcodes : unsigned short {
+    None,       Unknown,     Equal,      
     Plus,       Minus,       Mul,        
     Div,        Mod,         LT,         
     GT,         LEQ,         GEQ,        
@@ -55,15 +33,14 @@ enum lexem_code : unsigned short {
     Return,     Read,        Print,      
     Kw_bool,    Kw_string,   Kw_float,   
     Kw_int,     Kw_char,     Kw_void,    
-    Single,     Double,      Extended,   
-    Quatro,     
+    
 };
 
 struct Lexem_info{
-    lexem_code code;
+    lexemcodes code;
     union{
         size_t    ident_index;
-unsigned __int128 int_val;__float128 float_val;
+unsigned __int128 int_val; __float128 float_val;
     };
 };
 
